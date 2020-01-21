3a46fa2f274e40a26dd3b821004341f639edec75
M1CH43L <michaellsimeon@gmail.com>
2020-01-20 22:32:31 -0500

    Add intro screen

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import Starfield from "./Starfield";
 
 const canvas = document.getElementById("canvas");
 const key = document.getElementById("any-key-id");
+const intro = document.getElementById("intro");
+const instruct = document.getElementById("instructions-div");
 
 let ctx = canvas.getContext("2d");
 
@@ -16,6 +18,8 @@ starfield.animate(ctx);
 document.addEventListener("keypress", () => {
   starfield.animating = false;
   key.style.visibility = "hidden";
+  intro.style.visibility = "hidden";
+  instruct.style.visibility = "hidden";
   new Nebula(canvas);
 })
 
